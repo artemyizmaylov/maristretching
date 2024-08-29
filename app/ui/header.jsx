f2d465baf841e06e50649e41eb85347faaa31f18
Artemy Izmaylov <artemyizmaylov@gmail.com>
2024-08-29 10:44:15 +0300

    Добавлена карточка курса

--- app/ui/header.jsx
+++ app/ui/header.jsx
@@ -12,17 +12,19 @@ export default function Header() {
         ['Блог', '/blog'],
         ['Контакты', '/contacts'],
     ];
-    const style = 'hover:text-pink';
+    const linksStyle = 'hover:text-pink font-normal';
 
     return (
-        <header className="flex justify-between items-center border-b border-green py-4 page-x-padding">
-            <h1 className="text-green uppercase">Mari Stretching</h1>
-            <nav className="flex gap-11">
-                {links.map(([title, url]) => (
-                    <Link key={title} href={url} className={`${style} ${pathname === url ? 'text-pink underline' : ''}`}>{title}</Link>
-                ))}
-            </nav>
-            <Link href="#" className="text-green uppercase border border-green rounded-full p-4">Telegram</Link>
+        <header className="border-b border-green py-4 sticky top-0">
+            <div className="container flex justify-between items-center">
+                <h1 className="text-green uppercase text-3xl">Mari Stretching</h1>
+                <nav className="flex gap-11">
+                    {links.map(([title, url]) => (
+                        <Link key={title} href={url} className={`${linksStyle}${pathname === url ? ' text-pink underline' : ''}`}>{title}</Link>
+                    ))}
+                </nav>
+                <Link href="#" className="text-green uppercase border border-green rounded-full p-4">Telegram</Link>
+            </div>
         </header>
     );
 }
