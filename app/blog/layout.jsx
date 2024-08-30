4861a257c4b835c8860261966cc2956556b890bf
Artemy Izmaylov <artemyizmaylov@gmail.com>
2024-08-30 13:48:17 +0300

    Сверстан блог

--- app/blog/layout.jsx
+++ app/blog/layout.jsx
@@ -1,7 +1,6 @@
 export default function BlogLayout({ children }) {
     return (
         <main className="container">
-            <h1 className="uppercase text-green font-extralight text-7xl my-20">Блог</h1>
             {children}
         </main>
     );
--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -2,13 +2,16 @@ import ArticleCard from "../ui/article-card";
 
 export default function Blog() {
     return (
-        <div className="flex flex-wrap gap-y-[145px] justify-center lg:justify-between">
-            <ArticleCard />
-            <ArticleCard />
-            <ArticleCard />
-            <ArticleCard />
-            <ArticleCard />
-            <ArticleCard />
+        <div className="">
+            <h1 className="uppercase text-green font-extralight text-7xl my-20">Блог</h1>
+            <div className="flex flex-wrap gap-y-[145px] justify-center lg:justify-between">
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+            </div>
         </div>
     );
 }
