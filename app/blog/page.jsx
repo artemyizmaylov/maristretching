5784c7584540cf8e13d5ea53edf51f503f1c9a7b
Artemy Izmaylov <artemyizmaylov@gmail.com>
2025-05-14 20:31:57 +0300

    Добавлены страницы блога и курсов

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -7,7 +7,7 @@ export default function Blog() {
     return (
         <div className="pt-10 sm:pt-20">
             <h1 className="uppercase text-green font-extralight text-4xl sm:text-7xl mb-10 sm:mb-20">Блог</h1>
-            <div className="flex flex-wrap gap-10 sm:gap-20 justify-around">
+            <div className="flex flex-wrap gap-10 sm:gap-20 justify-center">
                 {posts.map((post) => (
                     <ArticleCard key={post.id} data={post} />
                 ))}
--- app/ui/course-card.jsx
+++ app/ui/course-card.jsx
@@ -8,7 +8,7 @@ export default function CourseCard({ data }) {
         <div className="container pb-12 max-w-[70%] md:max-w-[85%]">
             <h3 className="font-extralight text-4xl sm:text-7xl text-pink uppercase text-center pb-2 sm:pb-7 lg:pb-20">{data.title}</h3>
             <div className="flex flex-col-reverse lg:flex-row gap-2 sm:gap-7">
-                <div className="flex flex-col gap-4 sm:gap-7 justify-between basis-1/2">
+                <div className="flex flex-col gap-7 justify-between basis-1/2">
                     <p className="text-left">{data.annotation}</p>
                     <Link
                         href={
@@ -27,9 +27,9 @@ export default function CourseCard({ data }) {
                 </div>
                 <div className="flex flex-col gap-7 justify-between basis-1/2">
                     <Image src={data.image} alt="#" width={350} height={350} className="aspect-square object-cover hidden lg:block " />
-                    <ul className="flex flex-wrap">
+                    <ul className="hidden flex-wrap md:flex justify-center lg:justify-start">
                         {data.tags.map((item) => (
-                            <li className="pl-3 pr-3 first-of-type:border-l border-r border-l-green text-sm sm:text-xl" key={item}>{item}</li>
+                            <li className="pl-3 pr-3 first-of-type:border-l border-r border-l-green text-sm lg:text-xl" key={item}>{item}</li>
                         ))}
                     </ul>
                 </div>
