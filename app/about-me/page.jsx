b2ca7b3a1989ed191112ec55830d257cc1331aa6
Artemy Izmaylov <artemyizmaylov@gmail.com>
2025-05-19 10:19:14 +0300

    добавлен новый блок

--- app/about-me/page.jsx
+++ app/about-me/page.jsx
@@ -49,6 +49,93 @@ export default function AboutMe() {
                     </Link>
                 </div>
             </div>
+
+            {/* Блок "Мой путь" */}
+            <div className="mt-20 sm:mt-32">
+                <h2 className="text-pink font-extralight text-4xl sm:text-6xl lg:text-7xl uppercase pb-10 sm:pb-16">
+                    Мой путь
+                </h2>
+
+                <div className="relative">
+                    {/* Вертикальная линия таймлайна */}
+                    <div className="absolute left-4 sm:left-1/2 h-full w-0.5 bg-pink/20 -z-10"></div>
+
+                    {/* Элементы таймлайна */}
+                    <div className="space-y-12">
+                        {/* 2018 */}
+                        <div className="relative flex flex-col sm:flex-row items-start sm:items-center gap-6 sm:gap-12">
+                            <div className="sm:absolute left-4 sm:left-1/2 sm:-translate-x-1/2 -translate-y-1/2 flex items-center justify-center w-8 h-8 rounded-full bg-pink text-white font-medium text-xs">
+                                2018
+                            </div>
+                            <div className="sm:w-1/2 sm:pr-12 sm:mt-0 sm:text-right ml-12 sm:ml-0">
+                                <p className="text-lg sm:text-xl font-medium text-gray-900">Поступление в медицинский университет</p>
+                                <p className="text-gray-600">Начинаю проводить первые занятия в Петербурге</p>
+                            </div>
+                            <div className="sm:w-1/2 sm:pl-12 hidden sm:block"></div>
+                        </div>
+
+                        {/* 2019 */}
+                        <div className="relative flex flex-col sm:flex-row items-start sm:items-center gap-6 sm:gap-12">
+                            <div className="sm:absolute left-4 sm:left-1/2 sm:-translate-x-1/2 -translate-y-1/2 flex items-center justify-center w-8 h-8 rounded-full bg-pink text-white font-medium text-xs">
+                                2019
+                            </div>
+                            <div className="sm:w-1/2 sm:pr-12 hidden sm:block"></div>
+                            <div className="sm:w-1/2 sm:pl-12 ml-12 sm:ml-0">
+                                <p className="text-lg sm:text-xl font-medium text-gray-900">Участие в фитнес-проектах</p>
+                                <p className="text-gray-600">Работа в качестве тренера-наставника</p>
+                            </div>
+                        </div>
+
+                        {/* 2021 */}
+                        <div className="relative flex flex-col sm:flex-row items-start sm:items-center gap-6 sm:gap-12">
+                            <div className="sm:absolute left-4 sm:left-1/2 sm:-translate-x-1/2 -translate-y-1/2 flex items-center justify-center w-8 h-8 rounded-full bg-pink text-white font-medium text-xs">
+                                2021
+                            </div>
+                            <div className="sm:w-1/2 sm:pr-12 sm:mt-0 sm:text-right ml-12 sm:ml-0">
+                                <p className="text-lg sm:text-xl font-medium text-gray-900">Обучение по растяжке</p>
+                                <p className="text-gray-600">Освоение новых методик работы с телом</p>
+                            </div>
+                            <div className="sm:w-1/2 sm:pl-12 hidden sm:block"></div>
+                        </div>
+
+                        {/* 2022 */}
+                        <div className="relative flex flex-col sm:flex-row items-start sm:items-center gap-6 sm:gap-12">
+                            <div className="sm:absolute left-4 sm:left-1/2 sm:-translate-x-1/2 -translate-y-1/2 flex items-center justify-center w-8 h-8 rounded-full bg-pink text-white font-medium text-xs">
+                                2022
+                            </div>
+                            <div className="sm:w-1/2 sm:pr-12 hidden sm:block"></div>
+                            <div className="sm:w-1/2 sm:pl-12 ml-12 sm:ml-0">
+                                <p className="text-lg sm:text-xl font-medium text-gray-900">Развитие блога и обучение</p>
+                                <p className="text-gray-600">Инструктор по пилатесу, онлайн-занятия</p>
+                            </div>
+                        </div>
+
+                        {/* 2023 */}
+                        <div className="relative flex flex-col sm:flex-row items-start sm:items-center gap-6 sm:gap-12">
+                            <div className="sm:absolute left-4 sm:left-1/2 sm:-translate-x-1/2 -translate-y-1/2 flex items-center justify-center w-8 h-8 rounded-full bg-pink text-white font-medium text-xs">
+                                2023
+                            </div>
+                            <div className="sm:w-1/2 sm:pr-12 sm:mt-0 sm:text-right ml-12 sm:ml-0">
+                                <p className="text-lg sm:text-xl font-medium text-gray-900">Первый курс и двигательная терапия</p>
+                                <p className="text-gray-600">Выпуск курсов, углубление в терапию</p>
+                            </div>
+                            <div className="sm:w-1/2 sm:pl-12 hidden sm:block"></div>
+                        </div>
+
+                        {/* 2024 */}
+                        <div className="relative flex flex-col sm:flex-row items-start sm:items-center gap-6 sm:gap-12">
+                            <div className="sm:absolute left-4 sm:left-1/2 sm:-translate-x-1/2 -translate-y-1/2 flex items-center justify-center w-8 h-8 rounded-full bg-pink text-white font-medium text-xs">
+                                2024
+                            </div>
+                            <div className="sm:w-1/2 sm:pr-12 hidden sm:block"></div>
+                            <div className="sm:w-1/2 sm:pl-12 ml-12 sm:ml-0">
+                                <p className="text-lg sm:text-xl font-medium text-gray-900">Окончание университета и новые достижения</p>
+                                <p className="text-gray-600">100 000 подписчиков на YouTube, обучение нутрициологии, очные занятия</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
