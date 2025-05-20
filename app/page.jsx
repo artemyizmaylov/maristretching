c818502892802d7b1f25c515bd50843da76d741f
Artemy Izmaylov <artemyizmaylov@gmail.com>
2025-05-20 16:04:48 +0300

    Обновлен экран хиро

--- app/page.jsx
+++ app/page.jsx
@@ -29,7 +29,7 @@ export default async function Home() {
 
   return (
     <main className="py-20 px-4 relative">
-      <Image src={'/images/hero.webp'} alt="#" width={2560} height={1440} className="absolute top-0 left-0 w-full h-dvh -z-10 object-cover origin-right" />
+      <Image src={'/images/hero.webp'} alt="#" width={4531} height={3021} className="absolute top-0 left-0 w-full h-dvh -z-10 object-cover origin-right" />
 
       <section id="hero" className="relative w-full min-h-screen flex flex-col justify-center items-center sm:items-start px-4 sm:px-8 md:px-12 lg:px-20 py-20 gap-8 sm:gap-12 overflow-hidden">
         <div className="absolute -right-20 -top-20 w-64 h-64 rounded-full bg-pink/10 blur-3xl -z-10"></div>
