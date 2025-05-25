1b3a45f7f0c4318d5e80606f13d980a7af472ed4
Artemy Izmaylov <artemyizmaylov@gmail.com>
2025-05-25 13:32:42 +0300

    Реализована аутентификация через гугл

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -6,13 +6,23 @@ export const authOptions = {
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            authorization: {
+                params: {
+                    prompt: "select_account",
+                },
+            },
         }),
     ],
     callbacks: {
+        async signIn({ user }) {
+            const isAllowed = user.email === process.env.ALLOWED_USER_EMAIL;
+            return isAllowed ? true : false;
+        },
         async redirect({ url, baseUrl }) {
             if (url.startsWith("/create-post")) return url;
+            if (url.startsWith(baseUrl)) return `${baseUrl}/create-post`;
             return baseUrl;
-        }
+        },
     }
 };
 
