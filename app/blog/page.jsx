8b556389f9b8f984109132dbe8c24e25042cbe15
Artemy Izmaylov <artemyizmaylov@gmail.com>
2025-05-25 17:23:08 +0300

    Исправлены апи

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -4,7 +4,7 @@ export const dynamic = 'force-dynamic'; // Добавляем эту строк�
 
 async function loadPosts() {
     try {
-        const response = await fetch(`${process.env.NEXTAUTH_URL}/api/get-sorted-posts`);
+        const response = await fetch(`/api/get-sorted-posts`);
 
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
