1e5cf6052507554b6f772adfc558bb2bd4bc6bbe
Artemy Izmaylov <artemyizmaylov@gmail.com>
2026-01-05 22:26:38 +0300

    Визуальные улучшения

--- app/page.jsx
+++ app/page.jsx
@@ -32,7 +32,7 @@ export default async function Home() {
 
       <section id="hero" className="relative w-full h-screen flex flex-col justify-center items-center sm:items-start px-4 sm:px-8 md:px-12 lg:px-20 py-20 gap-8 sm:gap-12 overflow-hidden">
         <h1 className="font-extralight text-4xl sm:text-7xl text-green uppercase text-center sm:text-left animate-fadeIn">
-          Mari Stretching
+          Растяжка с Мари
         </h1>
         <p className="text-2xl max-w-[537px] text-center sm:text-left leading-relaxed animate-fadeIn delay-100">
           Я Мари и моя миссия - рассказывать о тренировках и здоровье из любви к себе.
@@ -46,32 +46,34 @@ export default async function Home() {
         </Link>
       </section>
 
-      <section className="max-w-7xl mx-auto py-16 sm:py-24">
+      <section className="max-w-7xl mx-auto">
         <div className="flex flex-col lg:flex-row gap-12 xl:gap-16 items-stretch">
           <div className="w-full lg:w-1/2 flex flex-col gap-8 sm:gap-10">
             <h2 className="text-pink font-extralight text-4xl sm:text-6xl lg:text-7xl uppercase leading-tight">
-              РАСТЯЖКА С МАРИ
+              РАЦИОН ПИТАНИЯ
             </h2>
-            <div className="space-y-10">
+            <div className="space-y-10 flex flex-col grow">
               <div className="group">
                 <h3 className="text-2xl sm:text-3xl lg:text-4xl mb-4 pb-2 border-b border-pink/20 group-hover:border-pink transition-colors duration-500">
-                  ОБО МНЕ
+                  РАЦИОН НА 30 ДНЕЙ
                 </h3>
                 <p className="text-base sm:text-lg lg:text-xl leading-relaxed text-gray-700">
-                  Меня зовут Мария Дягилева, и моя жизнь — это движение. С детства спорт был моей страстью, а стремление глубже понять тело и здоровье привело меня в медицинский университет. Теперь я врач, тренер и вечный ученик: учусь на нутрициолога, прохожу курсы, работаю с клиентами и веду группы в Петербурге. Пилатес, растяжка, восстановление после родов, МФР и двигательная терапия — мои инструменты для того, чтобы помогать людям чувствовать себя сильными и свободными в своем теле.
+                  - это готовое меню, где за вас уже продуманы блюда и сочетания продуктов. Каждый день у вас есть варианты завтрака, обеда, ужина и перекусов без сложных рецептов и редких ингредиентов. Меню построено  так, чтобы все купленные продукты использовались полностью. Каждая неделя сопровождается продуктовой корзиной, а к каждому блюду посчитано КБЖУ
                 </p>
               </div>
-              <div className="group">
+              <div className="group flex flex-col grow">
                 <h3 className="text-2xl sm:text-3xl lg:text-4xl mb-4 pb-2 border-b border-pink/20 group-hover:border-pink transition-colors duration-500">
-                  ФИЛОСОФИЯ
+                  РАЦИОН КАК ОПОРА
                 </h3>
-                <p className="text-base sm:text-lg lg:text-xl leading-relaxed text-gray-700">
-                  Но спорт — не просто физическая практика. Это философия, вдохновение и забота о себе. Поэтому я веду блог: на YouTube делюсь тренировками, а в Telegram — знаниями о здоровье и мотивацией. Для меня важно, чтобы движение приносило радость, а осознанность стала привычкой. Ведь гармония начинается с любви к себе — и я покажу, как ее найти.
+                <p className="text-base sm:text-lg lg:text-xl leading-relaxed text-gray-700 mb-6">
+                  Этот рацион задуман как опора, а не жёсткая диета. Вы просто открываете нужный день и следуете плану, добавляя к нему свои маленькие привычки и вкусы.
+                  После оплаты вы получаете эстетично оформленную книгу и доступ к рациону в закрытом телеграм‑канале. Там же можно присоединиться к общему чату: делиться фото блюд, задавать вопросы, вдохновляться и идти к новым пищевым привычкам вместе с другими девочками.
                 </p>
+                <a href="https://t.me/mari_stretching/2554" className="hover:text-green text-pink text-base sm:text-lg lg:text-xl leading-relaxed">Подробнее о рационе</a>
               </div>
             </div>
           </div>
-          <div className="w-full lg:w-1/2 relative overflow-hidden">
+          <div className="w-full lg:w-1/2 relative overflow-hidden flex justify-center">
             <Image
               src="/images/about-me1.webp"
               alt="Пилатес - польза для тела и разума"
